1a4d4dab0dabb91948ca0afe816259b6a0624169
Tariqul Islam <pathharatareq@gmail.com>
2019-04-27 23:22:47 +0530

    Add files via upload

---
+++ array.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+@arrNum =(1,2,3,4,5);
+@arrStr =("Apple","Orange","Pineapple");
+print "\$arrNum[0]= $arrNum[0]\n";
+print "\$arrNum[1]= $arrNum[1]\n";
+print "\$arrNum[2]= $arrNum[2]\n";
+print "\$arrNum[3]= $arrNum[3]\n";
+print "\$arrNum[4]= $arrNum[4]\n"; 
+print "\$arrStr[0]= $arrStr[0]\n";
+print "\$arrStr[1]= $arrStr[1]\n";
+print "\$arrStr[2]= $arrStr[2]\n";
---
+++ hash.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+%hashData = ('CSE', 134, 'IT', 133, 'ME', 154);
+%hashData1= ('Tareq','islam','Bidhan','roy','ME', 154,3,'hello');
+print "\$hashData{'CSE'}= $hashData{'CSE'}\n";
+print "\$hashData{'IT'}= $hashData{'IT'}\n";
+print "\$hashData{'ME'}= $hashData{'ME'}\n";
+print "\$hashData1{'Tareq'} = $hashData1{'Tareq'}\n";
+print "\$hashData1{'ME'} = $hashData1{'ME'}\n";
+print "\$hashData1{3} = $hashData1{3}\n";
